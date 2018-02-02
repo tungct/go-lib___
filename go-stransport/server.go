5433931fab1d13e980a6f7a5c06d0c5ba981ee4b
Cao-Tung <caotungjs@gmail.com>
2018-02-03 00:11:34 +0700

    init

---
+++ go-stransport/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"github.com/tungct/go-messqueue"
+	"encoding/json"
+	"fmt"
+	"github.com/tungct/go-workerpool"
+)
+
+func RecMessage(rw http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+
+	var message go_messqueue.Message
+	err := decoder.Decode(&message)
+
+	if err != nil {
+		panic(err)
+	}
+	go_messqueue.PutMessage(message)
+
+	fmt.Println(message.Content)
+}
+
+
+func main() {
+	go_messqueue.Queue = make(chan go_messqueue.Message, go_messqueue.MaxLenQueue)
+	go_workerpool.Worker = make(chan int, go_workerpool.MaxLenWorker)
+
+	for id := 0 ; id < go_workerpool.MaxLenWorker ; id ++{
+		go_workerpool.Worker <-id
+	}
+
+	// Worker execute message in pool, write to disk
+	go func() {
+		for {
+			w := <- go_workerpool.Worker
+			go_workerpool.WriteToDisk(w)
+		}
+	}()
+
+
+	router := mux.NewRouter()
+	router.HandleFunc("/message", RecMessage).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
---
+++ go-workerpool/workerpool.go
@@ -0,0 +1,34 @@
+package go_workerpool
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+	"io/ioutil"
+	"github.com/tungct/go-messqueue"
+)
+
+var MaxLenWorker int = 10
+// Worker pool
+var Worker chan(int)
+
+func WriteToDisk(id int) bool{
+	Worker <-id
+	message := <-go_messqueue.Queue
+	fmt.Println("Worker ", id, "execute Message")
+
+	// check exits file output
+	if _, err := os.Stat("output.json"); err == nil {
+		f, _ := os.OpenFile("output.json", os.O_APPEND|os.O_WRONLY, 0600)
+		rs, _ := json.Marshal(message)
+		if _, err := f.Write(rs); err != nil {
+			panic(err)
+		}
+		return true
+	}else {
+		jsonData, _  := json.Marshal(message)
+		ioutil.WriteFile("output.json", jsonData, 0600)
+		return true
+	}
+	return false
+}
